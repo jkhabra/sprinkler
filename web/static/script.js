532088e8537c51ab2f64db747327435a28b2d3c3
Jatinder Singh <jkhabra09@gmail.com>
2017-07-28 17:24:13 +0530

    Add spinner icon
    - Display message after publish

--- web/static/script.js
+++ web/static/script.js
@@ -6,6 +6,10 @@ for (let i = 0; i < allButtons.length; i++) {
   button.addEventListener('click', function(event) {
     let btn = event.target;
     let url = '/publish-photo?id=' + btn.dataset.photoId;
+    let parent = btn.parentNode;
+    parent.querySelector('.spinner').style.display='';
+    parent.querySelector('.thumb').style.background='black';
+    parent.querySelector('.post-image').style.opacity='0.5';
 
     fetch(url)
       .then(function(response) {
@@ -14,14 +18,24 @@ for (let i = 0; i < allButtons.length; i++) {
       .then(function(res) {
         console.warn(res);
 
-        if (res.status === 'ok') {
+        if (res.status === 'ok' || res.status === 'success') {
           console.log('Your post successfully posted to Facebook');
-        } else {
-          throw new Error('Failed to publish image');
+          parent.querySelector('.spinner').style.display='none';
+          parent.querySelector('.post-image').classList.add('thumb2');
+          parent.querySelector('.success').style.display='';
+        }
+        else {
+          throw new Error('Failed to publish image' || res.status === 'error');
+          parent.querySelector('.spinner').style.display='none';
+          parent.querySelector('.post-image').classList.add('thumb3');
+          parent.querySelector('.error').style.display='';
         }
       })
       .catch(function(error) {
         console.log('There has been a problem with your fetch operation: ' + error.message);
       });
+    btn.classList.add('disabled');
+    btn.setAttribute('disabled', 'disabled');
+
   });
 }
