40735fce51c5bc76e17a70a7daccd02540404a02
Jatinder Singh <jkhabra09@gmail.com>
2017-08-03 17:45:58 +0530

    Add images to side-bar

--- web/static/script.js
+++ web/static/script.js
@@ -1,7 +1,8 @@
 let allButtons = document.querySelectorAll('.publish-button');
 let state = window.state = {
   isBigPictureVisible: false,
-  allImages: document.querySelectorAll('.post-image')
+  allImages: document.querySelectorAll('.post-image'),
+  selectedImages: []
 };
 
 for (let i = 0; i < allButtons.length; i++) {
@@ -45,6 +46,7 @@ for (let i = 0; i < allButtons.length; i++) {
   });
 }
 
+// Make image bigger on click
 
 let allImages = document.querySelectorAll('.post-image');
 
@@ -76,22 +78,64 @@ for (let i = 0; i < allImages.length; i++) {
   });
 }
 
+// Display next image on click right button
+
 let rightSide = document.querySelector('.right');
 rightSide.addEventListener('click', function(event) {
   let right = event.target;
-  window.r = right;
   let currentImage = right.parentNode.querySelector('.b-image').src;
 
   for(let i = 0; i < allImages.length; i++) {
     if (allImages[i].src === currentImage){
       let bigImageIndex = i;
-      let y = allImages[bigImageIndex + 1].src;
-      let imagePath = new URL(y).pathname;
+      let next = allImages[bigImageIndex + 1].src;
+      let imagePath = new URL(next).pathname;
+
+      if (imagePath.endsWith('.mp4')) {
+        let mp4 = imagePath;
+        window.m = mp4;
+        right.parentNode.querySelector('.big-video').style.display='';
+        right.parentNode.querySelector('.b-image').style.display='none';
+        right.parentNode.querySelector('.big-video').src=mp4;
+      }
+      else {
+        right.parentNode.querySelector('.b-image').style.display='';
+        right.parentNode.querySelector('.big-video').style.display='none';
+      }
       right.parentNode.querySelector('.b-image').src=imagePath;
     }
   }
 });
 
+// Display previous image on click left button
+
+let leftSide = document.querySelector('.left');
+leftSide.addEventListener('click', function(event) {
+  let left = event.target;
+  let currentImage = left.parentNode.querySelector('.b-image').src;
+
+  for(let i = 0; i < allImages.length; i++) {
+    if (allImages[i].src === currentImage){
+      let bigImageIndex = i;
+      let previors = allImages[bigImageIndex - 1].src;
+      let imagePath = new URL(previors).pathname;
+
+      if (imagePath.endsWith('.mp4')) {
+        let mp4 = imagePath;
+        left.parentNode.querySelector('.big-video').style.display='';
+        left.parentNode.querySelector('.b-image').style.display='none';
+        left.parentNode.querySelector('.big-video').src=mp4;
+      }
+      else {
+        left.parentNode.querySelector('.b-image').style.display='';
+        left.parentNode.querySelector('.big-video').style.display='none';
+      }
+      left.parentNode.querySelector('.b-image').src=imagePath;
+    }
+  }
+});
+
+// Make big image's display none when click outside of target image
 
 document.body.addEventListener('click', function(event) {
   if (!state.isBigPictureVisible) {
@@ -105,7 +149,7 @@ document.body.addEventListener('click', function(event) {
     bigImg.style.display='none';
     state.isBigPictureVisible = false;
   }
-  if (target === bigImg.querySelector('.right')) {
+  if (target === bigImg.querySelector('.right') || target === bigImg.querySelector('.left') || target === bigImg.querySelector('.big-video')) {
     bigImg.style.display='';
     state.isBigPictureVisible = true;
   }
@@ -122,13 +166,44 @@ for (let i = 0; i < allMarks.length; i++) {
   mark.addEventListener('click', function(event) {
     let markElement = event.target;
 
+    // START Change selected-image checkbox
     if (markElement.dataset.isMarked === "true") {
       markElement.style.backgroundImage="url('/static/icons/un-mark.png')";
       markElement.dataset.isMarked = false;
-    } else {
+
+      // remove image from selected images state
+      let index = state.selectedImages.indexOf(markElement.parentNode.parentNode.querySelector('.post-image').src);
+      let remove = state.selectedImages.splice(index, 1);
+      document.querySelector('.side-options').innerHTML = makeSelectedImagesHtml(state.selectedImages);
+    }
+    else {
       markElement.style.backgroundImage="url('/static/icons/mark.png')";
       markElement.dataset.isMarked = true;
-    }
+      // END Change selected-image checkbox
 
+      // Add image to selected images state
+      let selectedImage = markElement.parentNode.parentNode.querySelector('.post-image').src;
+      state.selectedImages.push(selectedImage);
+
+      // Update sidebar with new selected images
+      document.querySelector('.side-options').innerHTML = makeSelectedImagesHtml(state.selectedImages);
+    }
   });
+}
+
+// function takes url and returns inner html
+function makeSelectedImagesHtml (urls) {
+  let html = '';
+
+  for (let i = 0; i < urls.length;  i++) {
+    let imageUrl = urls[i];
+
+    if (imageUrl.endsWith('.mp4')){
+      html += `<div class="side-image"><video muted class="small-image" src='${imageUrl}'"></video></div>`;
+    } else {
+      html += `<div class="side-image"><img class="small-image" src='${imageUrl}'></div>`;
+    }
+  }
+
+  return html;
 }
